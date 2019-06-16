9f92db8a8d8ba6ec3924b661bf663a0ac28166e1
Kyosuke Miyazawa <31465059+zenmai36rai@users.noreply.github.com>
2019-06-16 13:04:48 +0900

    g;++ Mahjong start up !

---
+++ mj.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string tehai1 = "4m5m6m4p5p6p4s5s6s東東中中";
+string tumo ="中";
+int main(){
+	cout << tehai1;
+	cout << "_";
+	cout << tumo;
+	cout << "\n";
+	return 0;
+};
