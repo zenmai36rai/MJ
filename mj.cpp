e5fc20d8919239558264a1a99439e3c060de21e2
Kyosuke Miyazawa <31465059+zenmai36rai@users.noreply.github.com>
2019-06-16 14:18:22 +0900

    Magic No. to String

--- mj.cpp
+++ mj.cpp
@@ -1,9 +1,35 @@
+#include <cstdio>
 #include <iostream>
 #include <string>
 using namespace std;
-string tehai1 = "4m5m6m4p5p6p4s5s6s東東中中";
-string tumo ="中";
+string tehai1;
+string tumo ="[ch]";
+int tehai_p1[13] = {4,5,6,13,14,15,22,23,24,27,27,33,33};
+char* int2hai(int i)
+{
+	static char kind[] = "mps";
+	static char tu_hai[] = "tonashpehkhtch";
+	static char buf[5];
+	if (i<27)
+	{
+		sprintf(buf,"[%d%c]",i%9,kind[i/9]);
+	} else {
+		int n = (i-27)*2;
+		sprintf(buf,"[%c%c]",tu_hai[n],tu_hai[n+1]);
+	}
+	return buf;
+};
+string vec2hai(int* v)
+{
+	static string ret;
+	for(int i=0;i<13; i++)
+	{
+		ret += int2hai(v[i]);
+	}
+	return ret;
+}
 int main(){
+	tehai1 = vec2hai(tehai_p1);
 	cout << tehai1;
 	cout << "_";
 	cout << tumo;
